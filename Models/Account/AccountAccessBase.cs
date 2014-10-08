999e5bbad914bc77f6caef251fdf6ed75c1d4dd7
steveoh <sgourley@utah.gov>
2014-10-07 19:53:46 -0600

    add emailaddress attribute

--- Models/Account/AccountAccessBase.cs
+++ Models/Account/AccountAccessBase.cs
@@ -1,9 +1,12 @@
-﻿namespace AgrcPasswordManagement.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgrcPasswordManagement.Models.Account
 {
     public class AccountAccessBase
     {
         public string Id { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
